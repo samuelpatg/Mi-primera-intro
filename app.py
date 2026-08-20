351bf9959ab7c04f3c62efe5f5e05426c9d786da
samuelpatg <samuelpatg@gmail.com>
2026-08-20 13:28:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ from PIL import Image
 st.title("Vapoi?")
 image = Image.open('vapoi.jpg')
 st.image(image, caption='Vapoi')
+
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El etxto escrito es', texto)
